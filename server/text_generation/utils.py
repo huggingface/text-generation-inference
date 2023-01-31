b4455b241b71af11acf0403cfd1e88f7c0848ce8
OlivierDehaene <23298448+OlivierDehaene@users.noreply.github.com>
2023-01-31 18:38:31 +0100

    update readme

--- server/text_generation/utils.py
+++ server/text_generation/utils.py
@@ -198,11 +198,10 @@ def try_to_load_from_cache(model_name, revision, filename):
 
     # Resolve refs (for instance to convert main to the associated commit sha)
     if refs_dir.is_dir():
-        for p in refs_dir.rglob("*"):
-            if str(p).endswith(revision):
-                with (refs_dir / revision).open() as f:
-                    revision = f.read()
-                break
+        revision_file = refs_dir / revision
+        if revision_file.exists():
+            with revision_file.open() as f:
+                revision = f.read()
 
     # Check if file is cached as "no_exist"
     if (no_exist_dir / revision / filename).is_file():
