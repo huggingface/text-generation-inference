03bdf18290b5552eab94d64e7cd954b373340b07
OlivierDehaene <olivier@huggingface.co>
2023-01-31 14:30:33 +0100

    fix(server): fix seeding on gpu (#42)

--- server/text_generation/models/causal_lm.py
+++ server/text_generation/models/causal_lm.py
@@ -63,7 +63,7 @@ class CausalLMBatch(Batch):
         for r in pb.requests:
             inputs.append(r.inputs)
             input_lengths.append(r.input_length)
-            next_token_choosers.append(NextTokenChooser.from_pb(r.parameters))
+            next_token_choosers.append(NextTokenChooser.from_pb(r.parameters, device))
             stopping_criterias.append(
                 StoppingCriteria.from_pb(r.stopping_parameters, tokenizer)
             )
