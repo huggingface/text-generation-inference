c0795de2f2c0beded136aa058f963dc1e341177a
OlivierDehaene <olivier@huggingface.co>
2023-03-09 13:00:10 +0100

    fix(server): do not warp prefill logits (#116)

--- server/text_generation_server/utils/tokens.py
+++ server/text_generation_server/utils/tokens.py
@@ -75,7 +75,11 @@ class NextTokenChooser:
 
     def __call__(self, input_ids, scores):
         # Warp logits
-        scores = self.warpers(input_ids, scores)
+        if scores.shape[0] > 1:
+            # only warp the last token logits
+            scores[-1:, :] = self.warpers(input_ids, scores[-1:, :])
+        else:
+            scores = self.warpers(input_ids, scores)
 
         # Compute logprobs
         logprobs = torch.log_softmax(scores, -1)
