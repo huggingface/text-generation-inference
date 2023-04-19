b6ee0ec7b06b11ad35fe2f2d99318ddc47f95558
OlivierDehaene <olivier@huggingface.co>
2023-04-19 21:36:59 +0200

    feat(router): add git sha to info route (#208)

--- router/build.rs
+++ router/build.rs
@@ -2,6 +2,18 @@ use std::error::Error;
 use vergen::EmitBuilder;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    EmitBuilder::builder().git_sha(false).emit()?;
+    // Try to get the git sha from the local git repository
+    if EmitBuilder::builder()
+        .fail_on_error()
+        .git_sha(false)
+        .emit()
+        .is_err()
+    {
+        // Unable to get the git sha
+        if let Ok(sha) = std::env::var("GIT_SHA") {
+            // Set it from an env var
+            println!("cargo:rustc-env=VERGEN_GIT_SHA={sha}");
+        }
+    }
     Ok(())
 }
