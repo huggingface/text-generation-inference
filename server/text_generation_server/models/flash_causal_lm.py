4096000e34ded704355c55fb2b3e1b0b3cb6b3ba
OlivierDehaene <olivier@huggingface.co>
2023-05-03 10:10:34 +0200

    fix(server): fix typo in tokenizers decode (#269)
    closes #268

--- server/text_generation_server/models/flash_causal_lm.py
+++ server/text_generation_server/models/flash_causal_lm.py
@@ -402,7 +402,7 @@ class FlashCausalLM(Model):
 
     def decode(self, generated_ids: Union[torch.Tensor, List[int]]) -> str:
         return self.tokenizer.decode(
-            generated_ids, skip_special_tokens=True, cleanup_tokenization_spaces=False
+            generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False
         )
 
     def forward(
