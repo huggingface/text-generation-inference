bef3458ee82233dad782acc955f8d8fdb6969316
Nicolas Patry <patry.nicolas@protonmail.com>
2023-05-10 12:30:28 +0200

    Update server/text_generation_server/models/__init__.py

--- server/text_generation_server/models/__init__.py
+++ server/text_generation_server/models/__init__.py
@@ -102,7 +102,7 @@ def get_model(
     config = AutoConfig.from_pretrained(model_id, revision=revision)
     model_type = config.model_type
 
-    if model_type == "gpt_bigcode": 
+    if model_type == "gpt_bigcode" or model_id.startswith("bigcode/"): 
         if sharded:
             if not FLASH_ATTENTION:
                 raise NotImplementedError(
