fix(server): fix init for flash causal lm (#352)
Fixes #347

server/text_generation_server/models/flash_neox.py
@@ -88,7 +88,7 @@ class FlashNeoXSharded(FlashNeoX):
     def load_weights(
         model,
         filenames: List[str],
-        quantize: bool,
+        quantize: Optional[str],
         device: torch.device,
         dtype: torch.dtype,
         rank: int,

server/text_generation_server/models/flash_santacoder.py
@@ -80,7 +80,7 @@ class FlashSantacoder(FlashCausalLM):
     def load_weights(
         model: FlashSantacoderForCausalLM,
         filenames: List[Path],
-        quantize: bool,
+        quantize: Optional[str],
         device: torch.device,
         dtype: torch.dtype,
         transpose: bool,

server/text_generation_server/models/opt.py
@@ -101,7 +101,7 @@ class OPTSharded(OPT):
     def load_weights(
         model,
         filenames: List[str],
-        quantize: bool,
+        quantize: Optional[str],
         device: torch.device,
         dtype: torch.dtype,
         rank: int,