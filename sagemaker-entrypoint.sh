1ba78207e6457ad2f7eba6ca145465bdd6ca2adf
Xin Yang <105740670+xyang16@users.noreply.github.com>
2023-05-22 01:10:21 -0700

    fix: set MODEL_ID in sagemaker-entrypoint script (#343)

--- sagemaker-entrypoint.sh
+++ sagemaker-entrypoint.sh
@@ -4,6 +4,7 @@ if [[ -z "${HF_MODEL_ID}" ]]; then
   echo "HF_MODEL_ID must be set"
   exit 1
 fi
+export MODEL_ID="${HF_MODEL_ID}"
 
 if [[ -n "${HF_MODEL_REVISION}" ]]; then
   export REVISION="${HF_MODEL_REVISION}"
