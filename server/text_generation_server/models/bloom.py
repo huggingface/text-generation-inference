bf7f1d54345f9e6e091d4b756e8b8381fe767d89
OlivierDehaene <23298448+OlivierDehaene@users.noreply.github.com>
2023-05-30 13:56:03 +0200

    fix(server): fix quantization

--- server/text_generation_server/models/bloom.py
+++ server/text_generation_server/models/bloom.py
@@ -245,14 +245,12 @@ class BLOOMSharded(BLOOM):
                                 return linear
 
                             module.linear = replace_linear(state)
-                        elif quantize == "gptq":
-                            raise NotImplementedError(
-                                "`gptq` is not implemented for now"
-                            )
-                        elif quantize is None:
-                            tensor = tensor.to(device)
-                        else:
-                            raise ValueError(f"Unexpected quantize `{quantize}`")
+                    elif quantize == "gptq":
+                        raise NotImplementedError("`gptq` is not implemented for now")
+                    elif quantize is None:
+                        tensor = tensor.to(device)
+                    else:
+                        raise ValueError(f"Unexpected quantize `{quantize}`")
 
                     module._parameters[param_name] = tensor
                     if name == "word_embeddings.weight":
