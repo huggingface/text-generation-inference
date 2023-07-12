38c2be5926e2f871e263481cb29f32e80a04684f
Felix Marty <9808326+fxmarty@users.noreply.github.com>
2023-07-12 18:31:49 +0000

    fix test

--- integration-tests/models/test_flash_starcoder_gptq.py
+++ integration-tests/models/test_flash_starcoder_gptq.py
@@ -2,55 +2,48 @@ import pytest
 
 
 @pytest.fixture(scope="module")
-def flash_santacoder_gptq_handle(launcher):
+def flash_starcoder_gptq_handle(launcher):
     with launcher("Narsil/starcoder-gptq", num_shard=2, quantize="gptq") as handle:
         yield handle
 
 
 @pytest.fixture(scope="module")
-async def flash_santacoder_gptq(flash_santacoder_gptq_handle):
-    await flash_santacoder_gptq_handle.health(300)
-    return flash_santacoder_gptq_handle.client
+async def flash_starcoder_gptq(flash_starcoder_gptq_handle):
+    await flash_starcoder_gptq_handle.health(300)
+    return flash_starcoder_gptq_handle.client
 
 
 @pytest.mark.asyncio
 @pytest.mark.private
-async def test_flash_santacoder_gptq(flash_santacoder_gptq, response_snapshot):
-    response = await flash_santacoder_gptq.generate(
-        'def sum(L: List[int]):\n"""Sums all elements from the list L."""', max_new_tokens=40, decoder_input_details=True
+async def test_flash_starcoder_gptq(flash_starcoder_gptq, response_snapshot):
+    response = await flash_starcoder_gptq.generate(
+        "def geometric_mean(L: List[float]):", max_new_tokens=20, decoder_input_details=True
     )
 
-    # assert response.details.generated_tokens == 10
+    assert response.details.generated_tokens == 20
     assert response == response_snapshot
 
 
 @pytest.mark.asyncio
 @pytest.mark.private
-async def test_flash_santacoder_gptq_all_params(flash_santacoder_gptq, response_snapshot):
-    response = await flash_santacoder_gptq.generate(
-        'def sum(L: List[int]):\n"""Sums all elements from the list L."""',
-        max_new_tokens=10,
-        repetition_penalty=1.2,
-        return_full_text=True,
-        stop_sequences=["test"],
-        temperature=0.5,
-        top_p=0.9,
-        top_k=10,
-        truncate=5,
-        typical_p=0.9,
-        watermark=True,
+async def test_flash_starcoder_gptq_default_params(flash_starcoder_gptq, response_snapshot):
+    response = await flash_starcoder_gptq.generate(
+        "def geometric_mean(L: List[float]):",
+        max_new_tokens=20,
+        temperature=0.2,
+        top_p=0.95,
         decoder_input_details=True,
         seed=0,
     )
-
-    #assert response.details.generated_tokens == 10
+    
+    assert response.details.generated_tokens == 20
     assert response == response_snapshot
 
 
 @pytest.mark.asyncio
 @pytest.mark.private
-async def test_flash_santacoder_gptq_load(flash_santacoder_gptq, generate_load, response_snapshot):
-    responses = await generate_load(flash_santacoder_gptq, 'def sum(L: List[int]):\n"""Sums all elements from the list L."""', max_new_tokens=10, n=4)
+async def test_flash_starcoder_gptq_load(flash_starcoder_gptq, generate_load, response_snapshot):
+    responses = await generate_load(flash_starcoder_gptq, "def geometric_mean(L: List[float]):", max_new_tokens=20, n=4)
 
     assert len(responses) == 4
     assert all([r.generated_text == responses[0].generated_text for r in responses])
