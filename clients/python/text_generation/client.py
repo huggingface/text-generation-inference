5161d4148e68162584b15ef957924fe7c87fccfd
Ciarán Byrne <ciaranby@gmail.com>
2023-07-14 12:00:59 +0100

    Change type hint for backward compatibility with python <3.9

--- clients/python/text_generation/client.py
+++ clients/python/text_generation/client.py
@@ -3,7 +3,7 @@ import requests
 
 from aiohttp import ClientSession, ClientTimeout
 from pydantic import ValidationError
-from typing import Dict, Optional, List, AsyncIterator, Iterator, Union
+from typing import Dict, Optional, List, AsyncIterator, Iterator, Union, Tuple
 
 from text_generation.types import (
     StreamResponse,
@@ -41,7 +41,7 @@ class Client:
         headers: Optional[Dict[str, str]] = None,
         cookies: Optional[Dict[str, str]] = None,
         timeout: int = 10,
-        cert: Optional[Union[str, tuple[str, str]]] = None,
+        cert: Optional[Union[str, Tuple[str, str]]] = None,
     ):
         """
         Args:
