a6057c407650bfdb026b2712b460edc293183447
OlivierDehaene <23298448+OlivierDehaene@users.noreply.github.com>
2023-07-24 10:41:24 +0200

    fix(server): fix exllama buffers

--- server/text_generation_server/utils/gptq/exllama.py
+++ server/text_generation_server/utils/gptq/exllama.py
@@ -12,6 +12,7 @@ def ext_make_q4(qweight, qzeros, scales, g_idx, device):
     )
 
 
+
 def ext_q4_matmul(x, q4, q4_width):
     """Matrix multiplication, returns x @ q4"""
     outshape = x.shape[:-1] + (q4_width,)
@@ -32,8 +33,15 @@ TEMP_STATE = None
 TEMP_DQ = None
 
 
+def set_device(device):
+    global DEVICE
+    DEVICE = device
+
+
 def create_exllama_buffers():
     global MAX_DQ, MAX_INNER, ACT_ORDER, DEVICE, TEMP_STATE, TEMP_DQ
+
+    assert DEVICE is not None, "call set_device first"
 
     if ACT_ORDER:
         # TODO: this should be set to rust side `max_total_tokens`, but TGI
