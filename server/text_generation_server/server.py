bce5e22444aab16a3e3cc0a9b7caeab5ad33b16e
Nicolas Patry <patry.nicolas@protonmail.com>
2023-08-17 14:38:49 +0200

    Adding Idefics multi modal model. (#842)
    Co-Authored-By: Victor Sanh <victorsanh@gmail.com>
    
    
    # What does this PR do?
    
    <!--
    Congratulations! You've made it this far! You're not quite done yet
    though.
    
    Once merged, your PR is going to appear in the release notes with the
    title you set, so make sure it's a great title that fully reflects the
    extent of your awesome contribution.
    
    Then, please replace this with a description of the change and which
    issue is fixed (if applicable). Please also include relevant motivation
    and context. List any dependencies (if any) that are required for this
    change.
    
    Once you're done, someone will review your PR shortly (see the section
    "Who can review?" below to tag some potential reviewers). They may
    suggest changes to make the code even better. If no one reviewed your PR
    after a week has passed, don't hesitate to post a new comment
    @-mentioning the same persons---sometimes notifications get lost.
    -->
    
    <!-- Remove if not applicable -->
    
    Fixes # (issue)
    
    
    ## Before submitting
    - [ ] This PR fixes a typo or improves the docs (you can dismiss the
    other checks if that's the case).
    - [ ] Did you read the [contributor
    guideline](https://github.com/huggingface/transformers/blob/main/CONTRIBUTING.md#start-contributing-pull-requests),
          Pull Request section?
    - [ ] Was this discussed/approved via a Github issue or the
    [forum](https://discuss.huggingface.co/)? Please add a link
          to it if that's the case.
    - [ ] Did you make sure to update the documentation with your changes?
    Here are the
    [documentation
    guidelines](https://github.com/huggingface/transformers/tree/main/docs),
    and
    [here are tips on formatting
    docstrings](https://github.com/huggingface/transformers/tree/main/docs#writing-source-documentation).
    - [ ] Did you write any new necessary tests?
    
    
    ## Who can review?
    
    Anyone in the community is free to review the PR once the tests have
    passed. Feel free to tag
    members/contributors who may be interested in your PR.
    
    <!-- Your PR will be replied to more quickly if you can figure out the
    right person to tag with @
    
    
    @OlivierDehaene OR @Narsil
    
     -->
    
    ---------
    
    Co-authored-by: Victor Sanh <victorsanh@gmail.com>

--- server/text_generation_server/server.py
+++ server/text_generation_server/server.py
@@ -14,7 +14,7 @@ from text_generation_server.interceptor import ExceptionInterceptor
 from text_generation_server.models import Model, get_model
 from text_generation_server.pb import generate_pb2_grpc, generate_pb2
 from text_generation_server.tracing import UDSOpenTelemetryAioServerInterceptor
-
+from text_generation_server.models.idefics_causal_lm import IdeficsCausalLMBatch
 
 class TextGenerationService(generate_pb2_grpc.TextGenerationServiceServicer):
     def __init__(self, model: Model, cache: Cache, server_urls: List[str]):
@@ -25,6 +25,7 @@ class TextGenerationService(generate_pb2_grpc.TextGenerationServiceServicer):
         if model.device.type == "cuda":
             # Force inference mode for the lifetime of TextGenerationService
             self._inference_mode_raii_guard = torch._C._InferenceMode(True)
+
 
     async def Info(self, request, context):
         return self.model.info
@@ -54,9 +55,14 @@ class TextGenerationService(generate_pb2_grpc.TextGenerationServiceServicer):
         return generate_pb2.FilterBatchResponse(batch=filtered_batch.to_pb())
 
     async def Warmup(self, request, context):
-        batch = self.model.batch_type.from_pb(
-            request.batch, self.model.tokenizer, self.model.dtype, self.model.device
-        )
+        if self.model.batch_type == IdeficsCausalLMBatch: #Hack, i would rather use kwargs in the `from_pb` call
+            batch = self.model.batch_type.from_pb(
+                request.batch, self.model.tokenizer, self.model.processor, self.model.dtype, self.model.device
+            )
+        else:
+            batch = self.model.batch_type.from_pb(
+                request.batch, self.model.tokenizer, self.model.dtype, self.model.device
+            )
         max_supported_total_tokens = self.model.warmup(batch)
 
         return generate_pb2.WarmupResponse(
@@ -64,9 +70,14 @@ class TextGenerationService(generate_pb2_grpc.TextGenerationServiceServicer):
         )
 
     async def Prefill(self, request, context):
-        batch = self.model.batch_type.from_pb(
-            request.batch, self.model.tokenizer, self.model.dtype, self.model.device
-        )
+        if self.model.batch_type == IdeficsCausalLMBatch: #Hack, i would rather use kwargs in the `from_pb` call
+            batch = self.model.batch_type.from_pb(
+                request.batch, self.model.tokenizer, self.model.processor, self.model.dtype, self.model.device
+            )
+        else:
+            batch = self.model.batch_type.from_pb(
+                request.batch, self.model.tokenizer, self.model.dtype, self.model.device
+            )
 
         generations, next_batch = self.model.generate_token(batch)
         self.cache.set(next_batch)
