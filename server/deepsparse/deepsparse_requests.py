6a739e51426785aa9c35b8b429986ae067ad305c
rsnm2 <rshaw@neuralmagic.com>
2023-08-22 02:59:18 +0000

    made service for deepsparse

---
+++ server/deepsparse/deepsparse_requests.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+from typing import List, Optional
+
+@dataclass
+class Request:
+    id: int
+    prompt: str
+
+@dataclass
+class Batch:
+    id: int
+    requests: List[Request]
+
+@dataclass
+class CachedBatch:
+    batch_id: int
+    request_ids: List[int]
+
+@dataclass
+class Generation:
+    request_id: int
+    generated_text: Optional[str]
+
+@dataclass  
+class PrefillRequest:
+    batch: Batch
+
+@dataclass
+class DecodeRequest:
+    batches: List[CachedBatch]
+
+@dataclass
+class FilterBatchRequest:
+    batch_id: int
