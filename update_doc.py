ae0775084a298b033413293040df63c75b7d1a52
Nicolas Patry <patry.nicolas@protonmail.com>
2023-09-27 10:52:07 +0000

    Should be green now.

--- update_doc.py
+++ update_doc.py
@@ -11,13 +11,20 @@ def main():
     output = subprocess.check_output(["text-generation-launcher", "--help"]).decode("utf-8")
     final_doc = f"```\n{output}\n```"
 
+    filename = "docs/source/basic_tutorials/launcher.md"
     if args.check:
-        with open("docs/source/basic_tutorials/launcher.md", "r") as f:
+        with open(filename, "r") as f:
             doc = f.read()
             if doc != final_doc:
+
+                tmp = "launcher.md"
+                with open(tmp, "w") as g:
+                    g.write(final_doc)
+                diff = subprocess.run(["diff",tmp, filename], capture_output=True).stdout.decode("utf-8")
+                print(diff)
                 raise Exception("Doc is not up-to-date, run `python update_doc.py` in order to update it")
     else:
-        with open("docs/source/basic_tutorials/launcher.md", "w") as f:
+        with open(filename, "w") as f:
             f.write(final_doc)
 
 if __name__ == "__main__":
