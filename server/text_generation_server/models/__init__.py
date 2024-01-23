885591acdbe37700b646fd679fdc19cab869795e
OlivierDehaene <23298448+OlivierDehaene@users.noreply.github.com>
2024-01-23 10:30:17 +0100

    feat: add support for golden gate

--- server/text_generation_server/models/__init__.py
+++ server/text_generation_server/models/__init__.py
@@ -52,6 +52,9 @@ try:
     from text_generation_server.models.flash_llama import (
         FlashLlama,
     )
+    from text_generation_server.models.flash_golden_gate import (
+        FlashGoldenGate,
+    )
     from text_generation_server.models.flash_santacoder import (
         FlashSantacoderSharded,
     )
@@ -304,6 +307,26 @@ def get_model(
             )
         elif sharded:
             raise NotImplementedError(FLASH_ATT_ERROR_MESSAGE.format("Sharded Llama"))
+        else:
+            return CausalLM(
+                model_id,
+                revision,
+                quantize=quantize,
+                dtype=dtype,
+                trust_remote_code=trust_remote_code,
+            )
+    if model_type == "golden_gate":
+        if FLASH_ATTENTION:
+            return FlashGoldenGate(
+                model_id,
+                revision,
+                quantize=quantize,
+                dtype=dtype,
+                trust_remote_code=trust_remote_code,
+                use_medusa=use_medusa,
+            )
+        elif sharded:
+            raise NotImplementedError(FLASH_ATT_ERROR_MESSAGE.format("Sharded Golden Gate"))
         else:
             return CausalLM(
                 model_id,
