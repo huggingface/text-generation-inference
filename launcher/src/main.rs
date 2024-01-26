66292828e6c26fa0c52996a52691aad20cb2b10b
drbh <david.richard.holtz@gmail.com>
2024-01-26 10:57:53 -0500

    fix: correct comment typo

--- launcher/src/main.rs
+++ launcher/src/main.rs
@@ -372,7 +372,8 @@ struct Args {
     #[clap(long, env)]
     tokenizer_config_path: Option<String>,
 
-    /// Display a lot of information about your runtime environment
+    /// The path to the tokenizer config file. This path is used to load the tokenizer configuration which may 
+    /// include a `chat_template`. If not provided, the default config will be used from the model hub.
     #[clap(long, short, action)]
     env: bool,
 }
