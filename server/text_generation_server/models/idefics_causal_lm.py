56e919e4593d0739eab67bcd42d02470c7ac4b97
drbh <david.richard.holtz@gmail.com>
2024-02-08 11:33:00 -0500

    fix: update all NextTokenChoosers

--- server/text_generation_server/models/idefics_causal_lm.py
+++ server/text_generation_server/models/idefics_causal_lm.py
@@ -114,7 +114,7 @@ class IdeficsCausalLMBatch(Batch):
         for i, r in enumerate(pb.requests):
             requests_idx_mapping[r.id] = i
             inputs.append(r.inputs)
-            next_token_choosers.append(NextTokenChooser.from_pb(r.parameters, device))
+            next_token_choosers.append(NextTokenChooser.from_pb(r.parameters, device, tokenizer))
             stopping_criteria = StoppingCriteria.from_pb(
                 r.stopping_parameters, tokenizer
             )
