c7caac47f816b7bc46cfb49029f868ff3c0f3372
drbh <david.richard.holtz@gmail.com>
2024-02-22 14:11:14 +0000

    fix: update snapshot

--- integration-tests/models/test_grammar_llama.py
+++ integration-tests/models/test_grammar_llama.py
@@ -89,7 +89,7 @@ async def test_flash_llama_grammar_json(flash_llama_grammar, response_snapshot):
     assert response.details.generated_tokens == 30
     assert (
         response.generated_text
-        == '{"firstName":"David","hobby":"Trees","lastName":"Holtz","numCats":2}'
+        == '{"firstName":"David","lastName":"Holtz","hobby":"Trees","numCats":2}'
     )
     assert response == response_snapshot
 
