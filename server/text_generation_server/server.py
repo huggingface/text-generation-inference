a42dc2027b4b4fe4501c2a0601b1a6328c5e6611
OlivierDehaene <23298448+OlivierDehaene@users.noreply.github.com>
2024-02-27 11:24:07 +0100

    update commit

--- server/text_generation_server/server.py
+++ server/text_generation_server/server.py
@@ -127,8 +127,6 @@ class TextGenerationService(generate_pb2_grpc.TextGenerationServiceServicer):
         )
 
     async def Decode(self, request, context):
-        from torch.profiler import profile, ProfilerActivity
-
         start = time.time_ns()
         if len(request.batches) == 0:
             raise ValueError("Must provide at least one batch")
@@ -151,9 +149,7 @@ class TextGenerationService(generate_pb2_grpc.TextGenerationServiceServicer):
             batch = batches[0]
             concat_ns = None
 
-        with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA]) as prefill_prof:
             generations, next_batch, timings = self.model.generate_token(batch)
-        prefill_prof.export_chrome_trace("new_decode.json")
         self.cache.set(next_batch)
 
         return generate_pb2.DecodeResponse(
