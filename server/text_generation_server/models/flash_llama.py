ad0b36bd2852c78bed6ebdbdd2967f4bd777fff3
Nicolas Patry <patry.nicolas@protonmail.com>
2024-05-24 10:02:35 +0000

    Fixing codellama loads by using purely `AutoTokenizer`.
    - The need for the slow tokenizer default stems from back
      when llama 1 was introduced and all the flags where not
      supported in `tokenizers`.
    
    - Fixes #1891

--- server/text_generation_server/models/flash_llama.py
+++ server/text_generation_server/models/flash_llama.py
@@ -3,7 +3,6 @@ import torch.distributed
 
 from opentelemetry import trace
 from transformers import AutoConfig, AutoTokenizer, GenerationConfig
-from transformers.models.llama import LlamaTokenizer
 from typing import Optional
 
 from text_generation_server.models import FlashCausalLM
@@ -41,22 +40,13 @@ class FlashLlama(FlashCausalLM):
         else:
             raise NotImplementedError("FlashLlama is only available on GPU")
 
-        try:
-            tokenizer = LlamaTokenizer.from_pretrained(
-                model_id,
-                revision=revision,
-                padding_side="left",
-                truncation_side="left",
-                trust_remote_code=trust_remote_code,
-            )
-        except Exception:
-            tokenizer = AutoTokenizer.from_pretrained(
-                model_id,
-                revision=revision,
-                padding_side="left",
-                truncation_side="left",
-                trust_remote_code=trust_remote_code,
-            )
+        tokenizer = AutoTokenizer.from_pretrained(
+            model_id,
+            revision=revision,
+            padding_side="left",
+            truncation_side="left",
+            trust_remote_code=trust_remote_code,
+        )
         try:
             generation_config = GenerationConfig.from_pretrained(
                 model_id, revision=revision, trust_remote_code=trust_remote_code
