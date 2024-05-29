a6f160352570e102da3e53741ffdb26daa6652bb
Nicolas Patry <patry.nicolas@protonmail.com>
2024-05-29 15:46:53 +0000

    Missing cohere.

--- server/text_generation_server/models/custom_modeling/flash_cohere_modeling.py
+++ server/text_generation_server/models/custom_modeling/flash_cohere_modeling.py
@@ -308,6 +308,7 @@ class FlashCohereAttention(torch.nn.Module):
                 self.kv_head_mapping,
                 self.softmax_scale,
                 block_tables,
+                None,
                 input_lengths,
                 max_s,
             )
