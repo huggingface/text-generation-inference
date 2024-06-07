1deb372564f3399b2fa85be5704260c3ef943e1a
drbh <david.richard.holtz@gmail.com>
2024-06-07 03:28:15 +0000

    fix: add adapter_data param to phi and neox

--- server/text_generation_server/models/custom_modeling/flash_phi_modeling.py
+++ server/text_generation_server/models/custom_modeling/flash_phi_modeling.py
@@ -400,6 +400,7 @@ class FlashPhiForCausalLM(torch.nn.Module):
         max_s: int,
         prefill_cache_indices: Optional[torch.Tensor],
         lm_head_indices: Optional[torch.Tensor] = None,
+        adapter_data: Optional[torch.Tensor] = None,
     ) -> torch.Tensor:
         hidden_states = self.model(
             input_ids,
