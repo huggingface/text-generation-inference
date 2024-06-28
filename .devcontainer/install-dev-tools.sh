8e25428713bdf26de8f9e2812c7ef4112d952ae8
Morgan Funtowicz <funtowiczmo@gmail.com>
2024-06-28 15:16:10 +0200

    Update devcontainer to remove clang and base image on PyTorch

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -3,7 +3,7 @@ apt-get update
 apt-get upgrade -y
 
 # install Linux tools and Python 3
-apt-get install software-properties-common wget curl clang cmake git libopenmpi-dev libssl-dev \
+apt-get install software-properties-common wget curl cmake gcc g++ git libopenmpi-dev libssl-dev nija-build \
     python3-dev python3-pip python3-wheel python3-setuptools -y
 
 # install Python packages
