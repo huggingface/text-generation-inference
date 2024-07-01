6265956bc4020365fbaecc61e5923db364a5c5f4
Wang, Yi <yi.a.wang@intel.com>
2024-07-01 20:32:54 +0800

    refine get xpu free memory/enable Qwen2/gemma2/gemma/phi in intel platform (#2132)
    * refine get xpu free memory
    
    Signed-off-by: Wang, Yi A <yi.a.wang@intel.com>
    
    * enable qwen2 in xpu
    
    Signed-off-by: Wang, Yi A <yi.a.wang@intel.com>
    
    * enable gemma/gemma2/phi in intel platform
    
    Signed-off-by: Wang, Yi A <yi.a.wang@intel.com>
    
    ---------
    
    Signed-off-by: Wang, Yi A <yi.a.wang@intel.com>

--- server/text_generation_server/utils/import_utils.py
+++ server/text_generation_server/utils/import_utils.py
@@ -1,6 +1,7 @@
 import torch
 from loguru import logger
 import subprocess
+import os
 
 
 def is_ipex_available():
@@ -21,10 +22,13 @@ def get_cuda_free_memory(device, memory_fraction):
 def get_xpu_free_memory(device, memory_fraction):
     total_memory = torch.xpu.get_device_properties(device).total_memory
     device_id = device.index
-    query = f"xpu-smi dump -d {device_id} -m 18 -n 1"
-    output = subprocess.check_output(query.split()).decode("utf-8").split("\n")
-    used_memory = float(output[1].split(",")[-1]) * 1024 * 1024
-    free_memory = int(total_memory * 0.95 - used_memory)
+    memory_fraction = float(os.getenv("XPU_MEMORY_FRACTION", "1.0"))
+    free_memory = max(
+        0,
+        int(
+            total_memory * 0.9 * memory_fraction - torch.xpu.memory_reserved(device_id)
+        ),
+    )
     return free_memory
 
 
