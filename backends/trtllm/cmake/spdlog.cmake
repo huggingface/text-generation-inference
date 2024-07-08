f53ffa886d1172dbbc338f0f2b25c3e2b097b2dd
Morgan Funtowicz <morgan@huggingface.co>
2024-07-08 22:06:49 +0000

    Specify which default log level to use depending on CMake build type

--- backends/trtllm/cmake/spdlog.cmake
+++ backends/trtllm/cmake/spdlog.cmake
@@ -1,4 +1,12 @@
 set(SPDLOG_USE_FMT ON)
+
+# Define the level at which SPDLOG_ compilation level is defined
+if(${CMAKE_BUILD_TYPE} STREQUAL "Debug")
+    add_compile_definitions(SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_DEBUG)
+else()
+    add_compile_definitions(SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_INFO)
+endif()
+
 fetchcontent_declare(
         spdlog
         GIT_REPOSITORY https://github.com/gabime/spdlog.git
