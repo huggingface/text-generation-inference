ecffeeb367417bd011ad1b7cd974f25b06e019f9
drbh <david.richard.holtz@gmail.com>
2024-07-11 10:42:58 -0400

    fix: append DONE message to chat stream (#2221)
    * fix: append DONE message to chat stream
    
    * fix: update completions endpoint

--- router/src/server.rs
+++ router/src/server.rs
@@ -813,6 +813,10 @@ async fn completions(
             }
         };
 
+        let stream = stream.chain(futures::stream::once(async {
+            Ok(Event::default().data("[DONE]"))
+        }));
+
         let sse = Sse::new(stream).keep_alive(KeepAlive::default());
         Ok((headers, sse).into_response())
     } else {
@@ -1172,6 +1176,11 @@ async fn chat_completions(
             span,
         )
         .await;
+
+        let response_stream = response_stream.chain(futures::stream::once(async {
+            Ok(Event::default().data("[DONE]"))
+        }));
+
         let sse = Sse::new(response_stream).keep_alive(KeepAlive::default());
         Ok((headers, sse).into_response())
     } else {
