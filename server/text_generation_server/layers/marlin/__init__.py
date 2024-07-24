457791f511772d1456e7322c465cf39986dc35be
Daniël de Kok <me@danieldk.eu>
2024-07-24 16:33:26 +0200

    Split up `layers.marlin` into several files (#2292)
    The marlin.py file was getting large, split it up.

---
+++ server/text_generation_server/layers/marlin/__init__.py
@@ -0,0 +1,20 @@
+from typing import List, Tuple
+
+import torch
+from text_generation_server.layers.marlin.fp8 import GPTQMarlinFP8Linear
+from text_generation_server.layers.marlin.gptq import (
+    GPTQMarlinLinear,
+    GPTQMarlinWeight,
+    can_use_gptq_marlin,
+    repack_gptq_for_marlin,
+)
+from text_generation_server.layers.marlin.marlin import MarlinWeightsLoader
+
+__all__ = [
+    "GPTQMarlinFP8Linear",
+    "GPTQMarlinLinear",
+    "GPTQMarlinWeight",
+    "MarlinWeightsLoader",
+    "can_use_gptq_marlin",
+    "repack_gptq_for_marlin",
+]
