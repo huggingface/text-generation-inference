09868355486e1d68cf869efd3b2b06716b40251c
drbh <david.richard.holtz@gmail.com>
2024-07-29 16:58:42 +0000

    fix: remove global model id

--- server/text_generation_server/server.py
+++ server/text_generation_server/server.py
@@ -30,7 +30,7 @@ except (ImportError, NotImplementedError):
 
 from text_generation_server.pb import generate_pb2_grpc, generate_pb2
 from text_generation_server.tracing import UDSOpenTelemetryAioServerInterceptor
-from text_generation_server.models.globals import set_model_id, set_adapter_to_index
+from text_generation_server.models.globals import set_adapter_to_index
 
 
 class SignalHandler:
@@ -271,7 +271,6 @@ def serve(
         while signal_handler.KEEP_PROCESSING:
             await asyncio.sleep(0.5)
 
-    set_model_id(model_id)
     asyncio.run(
         serve_inner(
             model_id,
