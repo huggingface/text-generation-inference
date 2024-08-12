93c061ac79a91077bf3260b9c891540cb26c8354
Nicolas Patry <patry.nicolas@protonmail.com>
2024-08-12 17:53:26 +0200

    Updating the flake.

---
+++ _server.nix
@@ -0,0 +1,12 @@
+{ mkPoetryApplication, pkg-config, protobuf, openssl }:
+
+mkPoetryApplication {
+  # name = "text-generation-server";
+
+  projectDir = ./server;
+
+  # nativeBuildInputs = [ pkg-config ];
+
+  # buildInputs = [ openssl.dev protobuf ];
+
+}
