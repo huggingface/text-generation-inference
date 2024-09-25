e3e483c9011c418c4a024cb56a7cf94ed5b641a0
Daniël de Kok <me@danieldk.eu>
2024-09-25 10:27:29 +0000

    Add the usual model tests

--- integration-tests/models/test_flash_phi35_moe.py
+++ integration-tests/models/test_flash_phi35_moe.py
@@ -2,38 +2,74 @@ import pytest
 
 
 @pytest.fixture(scope="module")
-def flash_phi35_moe_chat_handle(launcher):
+def flash_phi35_moe_handle(launcher):
     with launcher(
-        "microsoft/Phi-3.5-MoE-instruct", num_shard=4, cuda_graphs=[1, 2]
+        "microsoft/Phi-3.5-MoE-instruct",
+        num_shard=4,
     ) as handle:
         yield handle
 
 
 @pytest.fixture(scope="module")
-async def flash_phi35_moe_chat(flash_phi35_moe_chat_handle):
-    await flash_phi35_moe_chat_handle.health(300)
-    return flash_phi35_moe_chat_handle.client
+async def flash_phi35_moe(flash_phi35_moe_handle):
+    await flash_phi35_moe_handle.health(300)
+    return flash_phi35_moe_handle.client
 
 
-@pytest.mark.private
-async def test_flash_phi35_moe_simple(flash_phi35_moe_chat, response_snapshot):
-    response = await flash_phi35_moe_chat.chat(
-        max_tokens=100,
-        seed=1337,
-        messages=[
-            {
-                "role": "system",
-                "content": "Youre a helpful assistant! Answer the users question best you can.",
-            },
-            {
-                "role": "user",
-                "content": "What is the weather like in Brooklyn, New York?",
-            },
-        ],
+@pytest.mark.asyncio
+async def test_flash_phi35_moe(flash_phi35_moe, response_snapshot):
+    response = await flash_phi35_moe.generate(
+        "What is gradient descent?\n\n", max_new_tokens=10, decoder_input_details=True
     )
 
+    assert response.details.generated_tokens == 10
     assert (
-        response.choices[0].message.content
-        == "I'm an AI unable to provide real-time data, but I can guide you on how to find current weather conditions in Brooklyn, New York. You can check websites like weather.com or accuweather.com, or use apps like The Weather Channel or AccuWeather on your smartphone. Alternatively, you can ask your voice assistant like Google Assistant or Siri for real-time updates.\n\nFor your information, I hope you'll have a"
+        response.generated_text
+        == "Gradient descent is a first-order optimization algorithm"
     )
     assert response == response_snapshot
+
+
+@pytest.mark.asyncio
+async def test_flash_phi35_moe_all_params(flash_phi35_moe, response_snapshot):
+    response = await flash_phi35_moe.generate(
+        "What is gradient descent?\n\n",
+        max_new_tokens=10,
+        repetition_penalty=1.2,
+        return_full_text=True,
+        stop_sequences=["test"],
+        temperature=0.5,
+        top_p=0.9,
+        top_k=10,
+        truncate=5,
+        typical_p=0.9,
+        watermark=True,
+        decoder_input_details=True,
+        seed=0,
+    )
+
+    assert response.details.generated_tokens == 10
+    assert (
+        response.generated_text
+        == "What is gradient descent?\n\nHello! It seems you're addressing a"
+    )
+    assert response == response_snapshot
+
+
+@pytest.mark.asyncio
+async def test_flash_phi35_moe_load(flash_phi35_moe, generate_load, response_snapshot):
+    responses = await generate_load(
+        flash_phi35_moe, "What is gradient descent?\n\n", max_new_tokens=10, n=4
+    )
+
+    assert len(responses) == 4
+    assert responses[0].details.generated_tokens == 10
+    assert (
+        responses[0].generated_text
+        == "Gradient descent is a first-order optimization algorithm"
+    )
+    assert all(
+        [r.generated_text == responses[0].generated_text for r in responses]
+    ), f"{[r.generated_text  for r in responses]}"
+
+    assert responses == response_snapshot
