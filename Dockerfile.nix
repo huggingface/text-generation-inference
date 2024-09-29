0b80a928eb85e770ad357210f0c12f3e61c9eb2d
Daniël de Kok <me@danieldk.eu>
2024-09-29 12:43:55 +0200

    Don't spam journalctl on Linux

--- Dockerfile.nix
+++ Dockerfile.nix
@@ -1,7 +1,7 @@
 # Build the image and get out the docker file:
 #
 # docker build -t tgi-nix-builder -f Dockerfile.nix
-# docker run tgi-nix-builder | docker load
+# docker run --log-driver=none tgi-nix-builder | docker load
 
 FROM nixos/nix:2.18.8
 RUN echo "experimental-features = nix-command flakes" >> /etc/nix/nix.conf
