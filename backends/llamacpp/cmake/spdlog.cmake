7d1f8a2bd6695be7a3efd6512c70093c6ae22d6d
Morgan Funtowicz <funtowiczmo@gmail.com>
2024-10-03 15:25:15 +0200

    feat(llamacpp): correctly handle CMAKE_BUILD_TYPE for spdlog macros

--- backends/llamacpp/cmake/spdlog.cmake
+++ backends/llamacpp/cmake/spdlog.cmake
@@ -3,7 +3,7 @@ set(SPDLOG_BUILD_SHARED OFF)
 set(SPDLOG_FMT_EXTERNAL ON)
 
 # Define the level at which SPDLOG_ compilation level is defined
-if (${CMAKE_BUILD_TYPE} STREQUAL "Debug")
+if (CMAKE_BUILD_TYPE STREQUAL "Debug")
     add_compile_definitions(SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_DEBUG)
 else ()
     add_compile_definitions(SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_INFO)
