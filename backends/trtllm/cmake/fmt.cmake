0745f2bf4f89f6509cdd7882f79ee16bbf2a8bf9
Morgan Funtowicz <funtowiczmo@gmail.com>
2024-10-18 00:07:53 +0200

    chore(cmake): download timestamp should be before URL

--- backends/trtllm/cmake/fmt.cmake
+++ backends/trtllm/cmake/fmt.cmake
@@ -1,6 +1,6 @@
 FetchContent_Declare(
         fmt
-        URL https://github.com/fmtlib/fmt/archive/refs/tags/11.0.2.tar.gz
         DOWNLOAD_EXTRACT_TIMESTAMP
+        URL https://github.com/fmtlib/fmt/archive/refs/tags/11.0.2.tar.gz
 )
 FetchContent_MakeAvailable(fmt)
--- backends/trtllm/cmake/spdlog.cmake
+++ backends/trtllm/cmake/spdlog.cmake
@@ -11,7 +11,7 @@ endif ()
 
 fetchcontent_declare(
         spdlog
-        URL https://github.com/gabime/spdlog/archive/refs/tags/v1.14.1.tar.gz
         DOWNLOAD_EXTRACT_TIMESTAMP
+        URL https://github.com/gabime/spdlog/archive/refs/tags/v1.14.1.tar.gz
 )
 fetchcontent_makeavailable(spdlog)
