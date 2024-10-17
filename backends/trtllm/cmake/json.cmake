09e88039b357a143a982a2b73559f7092668337d
Morgan Funtowicz <funtowiczmo@gmail.com>
2024-10-17 13:12:34 +0200

    chore(cmake): use correct policy for download_timestamp

--- backends/trtllm/cmake/json.cmake
+++ backends/trtllm/cmake/json.cmake
@@ -1,5 +1,6 @@
 fetchcontent_declare(
         json
         URL https://github.com/nlohmann/json/releases/download/v3.11.3/json.tar.xz
+        DOWNLOAD_EXTRACT_TIMESTAMP
 )
 fetchcontent_makeavailable(json)
