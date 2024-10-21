fa89d1e613c6f8971e14d84aba821d20984967cd
Morgan Funtowicz <funtowiczmo@gmail.com>
2024-10-21 09:14:35 +0200

    misc(cmake): wut

--- backends/llamacpp/src/lib.rs
+++ backends/llamacpp/src/lib.rs
@@ -1,6 +1,6 @@
 pub mod backend;
 
-#[cxx::bridge(namespace = "huggingface::tgi::backends::llama")]
+#[cxx::bridge(namespace = "huggingface::tgi::backends::llama::impl")]
 mod ffi {
     unsafe extern "C++" {
         include!("backends/llamacpp/csrc/backend.cpp");
