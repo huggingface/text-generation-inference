c6281a4893fd3050e8884489aa81be09f565848a
Daniël de Kok <me@danieldk.eu>
2024-10-24 15:29:33 +0000

    Switch from fbgemm-gpu w8a8 scaled matmul to vLLM/marlin-kernels
    Performance and accuracy of these kernels are on par (tested with Llama
    70B and 405B). Removes a dependency and resolves some stability issues
    we have been seeing.

--- server/text_generation_server/models/__init__.py
+++ server/text_generation_server/models/__init__.py
@@ -410,12 +410,6 @@ def get_model(
             else:
                 # These quantizers only work with float16 params.
                 dtype = torch.float16
-        elif quantize == "fp8":
-            from text_generation_server.layers.fp8 import FBGEMM_DYN_AVAILABLE
-
-            if FBGEMM_DYN_AVAILABLE:
-                # fbgemm kernels are fp8xfp8->bf16
-                dtype = torch.bfloat16
         else:
             # Keep it as default for now and let
             # every model resolve their own default dtype.
