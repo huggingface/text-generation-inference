5bab8c936851ff1dde489d52bec3a92aa471837a
Nicolas Patry <patry.nicolas@protonmail.com>
2024-10-28 08:30:45 +0100

    More timeout on docker start ?

--- integration-tests/conftest.py
+++ integration-tests/conftest.py
@@ -557,7 +557,7 @@ def launcher(event_loop):
             devices=devices,
             volumes=volumes,
             ports={"80/tcp": port},
-            healthcheck={"timeout": int(10 * 1e9)},
+            healthcheck={"timeout": int(60 * 1e9), "retries": 2},  # 60s
             shm_size="1G",
         )
 
