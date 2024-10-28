90b226db291769a45ecbccaa4f7384bc6b9bff8a
Nicolas Patry <patry.nicolas@protonmail.com>
2024-10-28 05:00:24 +0100

    We can have a tokenizer anywhere. (#2527)
    * We can have a tokenizer anywhere.
    
    * Handling potential lack of offsets (python tokenizer)
    
    * Remove redundancy.
    
    * Fixing the tests.
    
    * Flake.lock update ?
    
    * Fixing the  GIL locking.
    
    * Fixing mamba by using the transformers version.
    
    * Adding the legacy handle.
    
    * Ellide lifetime.
    
    * Lint.
    
    * Deprecation message.
    
    * Fixing bad rebase.

--- server/text_generation_server/models/__init__.py
+++ server/text_generation_server/models/__init__.py
@@ -226,7 +226,7 @@ class ModelType(enum.Enum):
         "url": "https://huggingface.co/databricks/dbrx-instruct",
     }
     MAMBA = {
-        "type": "ssm",
+        "type": "mamba",
         "name": "Mamba",
         "url": "https://huggingface.co/state-spaces/mamba-2.8b-slimpj",
     }
@@ -617,6 +617,10 @@ def get_model(
             speculator=speculator,
             dtype=dtype,
             trust_remote_code=trust_remote_code,
+        )
+    elif model_type == "ssm":
+        raise RuntimeError(
+            "`ssm` models have been deprecated in favor of `mamba` models, which follow standard HF formats. Check out a list here: https://huggingface.co/models?search=mamba%20-hf"
         )
 
     if model_id.startswith("facebook/galactica"):
