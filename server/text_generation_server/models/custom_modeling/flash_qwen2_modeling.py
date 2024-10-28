831a07f9900520bdee7cd1f694680b9cc84d29f2
David Holtz <david.richard.holtz@gmail.com>
2024-10-28 16:33:07 +0000

    fix: remove unused rotate_half

--- server/text_generation_server/models/custom_modeling/flash_qwen2_modeling.py
+++ server/text_generation_server/models/custom_modeling/flash_qwen2_modeling.py
@@ -49,13 +49,6 @@ def _load_gqa(config, prefix: str, weights):
     )
 
 
-def rotate_half(x):
-    """Rotates half the hidden dims of the input."""
-    x1 = x[..., : x.shape[-1] // 2]
-    x2 = x[..., x.shape[-1] // 2 :]
-    return torch.cat((-x2, x1), dim=-1)
-
-
 class Qwen2Attention(torch.nn.Module):
     def __init__(
         self,
