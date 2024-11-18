e0018723fc974cd6e85fd025d10f53a6e3ee928d
Daniël de Kok <me@danieldk.eu>
2024-11-18 12:43:12 +0000

    Use marlin-kernels 0.3.5

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
       inputs.nixpkgs.follows = "tgi-nix/nixpkgs";
     };
     nix-filter.url = "github:numtide/nix-filter";
-    tgi-nix.url = "github:huggingface/text-generation-inference-nix";
+    tgi-nix.url = "github:huggingface/text-generation-inference-nix/marlin-kernels-0.3.5";
     nixpkgs.follows = "tgi-nix/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
