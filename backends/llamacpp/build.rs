960c12bd6e4e57132ab39e4d1dee0df8e54b7506
Morgan Funtowicz <funtowiczmo@gmail.com>
2025-01-31 22:13:59 +0100

    backend(llama): add CUDA Dockerfile_llamacpp for now

--- backends/llamacpp/build.rs
+++ backends/llamacpp/build.rs
@@ -1,7 +1,29 @@
+use std::collections::HashMap;
 use std::env;
 use std::path::PathBuf;
 
+fn inject_transient_dependencies(lib_search_path: Option<&str>, lib_target_hardware: &str) {
+    let hardware_targets = HashMap::from([("cpu", None), ("cuda", Some(vec!["cuda"]))]);
+
+    if let Some(lib_search_path) = lib_search_path {
+        lib_search_path.split(":").for_each(|path| {
+            println!("cargo:rustc-link-search=dependency={path}");
+        });
+    }
+
+    if let Some(hardware_transient_deps) = hardware_targets.get(lib_target_hardware) {
+        if let Some(additional_transient_deps) = hardware_transient_deps {
+            additional_transient_deps.iter().for_each(|dep| {
+                println!("cargo:rustc-link-lib={dep}");
+            });
+        }
+    }
+}
+
 fn main() {
+    let lib_search_path = option_env!("TGI_LLAMA_LD_LIBRARY_PATH");
+    let lib_target_hardware = option_env!("TGI_LLAMA_HARDWARE_TARGET").unwrap_or("cpu");
+
     let bindings = bindgen::Builder::default()
         .header("src/wrapper.h")
         .prepend_enum_name(false)
@@ -16,5 +38,5 @@ fn main() {
 
     pkg_config::Config::new().probe("llama").unwrap();
 
-    println!("cargo::rerun-if-changed=build.rs");
+    inject_transient_dependencies(lib_search_path, lib_target_hardware);
 }
