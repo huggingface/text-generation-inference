0a55bd3db9704686c98a36f87263daa3843ee27f
Adrien Gallouët <angt@huggingface.co>
2025-02-20 15:40:40 +0000

    Quantize without llama-quantize
    Signed-off-by: Adrien Gallouët <angt@huggingface.co>

--- backends/llamacpp/make-gguf.sh
+++ backends/llamacpp/make-gguf.sh
@@ -5,14 +5,16 @@
 	return 1
 }
 
+case "$1" in (*?.gguf) ;; (*)
+	echo "Not a valid GGUF file: $1"
+	return 1;
+esac
+
 GGUF="$1"
-GGUF_DIR=$(dirname "$GGUF")
-GGUF_TMP="model.src/tmp.gguf"
+GGUF_DIR=$(dirname -- "$GGUF")
 MODEL_ID="$2"
 MODEL_DIR="model.src/$2"
 REV="${3-main}"
-
-[ -e "$GGUF" ] && return
 
 mkdir -p model.src "$GGUF_DIR"
 
@@ -22,12 +24,7 @@ huggingface-cli download \
 	"$MODEL_ID" &&
 
 convert_hf_to_gguf.py \
-	--outfile "$GGUF_TMP" \
-	"$MODEL_DIR" &&
+	--outfile "$GGUF" \
+	"$MODEL_DIR"
 
-llama-quantize \
-	"$GGUF_TMP" \
-	"$GGUF" \
-	"Q4_0"
-
-rm -rf model.src
+rm -rf -- model.src
