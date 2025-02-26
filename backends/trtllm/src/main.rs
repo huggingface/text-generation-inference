1ebee925ffeeea492b357aa270c1d2949c256723
Hugo Larcher <hugo.larcher@huggingface.co>
2025-02-26 22:11:08 +0100

    feat: add support for HF_HUB_USER_AGENT_ORIGIN to add user-agent Origin field in Hub requests.

--- backends/trtllm/src/main.rs
+++ backends/trtllm/src/main.rs
@@ -86,6 +86,10 @@ async fn get_tokenizer(tokenizer_name: &str, revision: Option<&str>) -> Option<T
             builder = builder.with_cache_dir(cache_dir.into());
         }
 
+        if let Some(origin) = std::env::var("HF_HUB_USER_AGENT_ORIGIN") {
+            builder = builder.with_user_agent("origin", origin);
+        }
+
         builder
     };
 
