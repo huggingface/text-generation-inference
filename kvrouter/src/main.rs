2a86538546b5da174aab0cc2b0bb27e509337f85
Corentin REGAL <corentin@huggingface.co>
2025-03-31 16:14:12 +0200

    add Dockerfile for kvrouter add router to set backends

--- kvrouter/src/main.rs
+++ kvrouter/src/main.rs
@@ -1,18 +1,23 @@
+use std::sync::Arc;
+
 use axum::{
     routing::Router,
     routing::{get, post},
 };
-use kvrouter::{handler, Communicator, ContentAware, OverloadHandler, RoundRobin};
+use kvrouter::{
+    handler, set_backends_handler, Communicator, ContentAware, OverloadHandler, RoundRobin,
+};
+use tokio::sync::RwLock;
 
 #[tokio::main]
 async fn main() {
     // List of backend servers
-    let backends = vec![
-        "http://localhost:8000".to_string(),
+    let backends = Arc::new(RwLock::new(vec![
+        // "http://localhost:8000".to_string(),
         // "http://localhost:8001".to_string(),
         // "http://localhost:8002".to_string(),
         // "http://localhost:8003".to_string(),
-    ];
+    ]));
 
     // Create a new instance of the RoundRobinRouter
 
@@ -25,17 +30,18 @@ async fn main() {
         if std::env::var("TGI_KVROUTER_LB").unwrap_or("".to_string()) == *"roundrobin" {
             println!("Using round robin");
             let lb = RoundRobin::new();
-            let mut router = OverloadHandler::new(lb, backends, rx);
+            let mut router = OverloadHandler::new(lb, backends, rx).await;
             router.run().await;
         } else {
             let lb = ContentAware::new();
-            let mut router = OverloadHandler::new(lb, backends, rx);
+            let mut router = OverloadHandler::new(lb, backends, rx).await;
             router.run().await;
         };
     });
     let app = Router::new()
         .route("/{*key}", get(handler))
         .route("/{*key}", post(handler))
+        .route("/_kvrouter/set-backends", post(set_backends_handler))
         .with_state(communicator);
 
     // run it
